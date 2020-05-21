8435c1b0e63d56e1a7f0df846ebac1118e57aa35
wanhar <wanhar@sehatq.com>
2020-05-21 13:51:56 +0700

    master

---
+++ Todolist/Managers/CoreDataManager.swift
@@ -0,0 +1,14 @@
+//
+//  CoreDataManager.swift
+//  Todolist
+//
+//  Created by Wanhar on 21/05/20.
+//  Copyright © 2020 Wanhar. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataManager {
+    <#code#>
+}

d3bc8a285863ea6fcd2e9ba55675a76d9c4287e0
wanhar <wanhar@sehatq.com>
2020-05-21 13:38:29 +0700

    Initial Commit

---
+++ Todolist/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Todolist
+//
+//  Created by Wanhar on 21/05/20.
+//  Copyright © 2020 Wanhar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
